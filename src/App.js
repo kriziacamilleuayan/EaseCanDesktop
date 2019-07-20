aa486c85a2d8257d6581276505ea132770706fe8
jelmarose <jelmarosegracedevera@gmail.com>
2019-07-21 06:43:29 +0800

    Added splash screen

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import "./App.css";
 import CameraFrame from "./components/camera/CameraFrame";
 import AppManager from "./components/appmanager/AppManager";
 import Spinner from "./components/spinner/Spinner";
+import Splash from './components/splash/Splash'
+import Reviews from './components/reviews/Reviews'
 
 export default class App extends Component {
+  state = {
+    viewSplash: true
+  }
+  hideSplash = () => {
+    this.setState({ viewSplash: false })
+  }
   render() {
     return (
       <div className="App">
-        <AppManager>
-          <CameraFrame />
-        </AppManager>
+        {this.state.viewSplash &&
+          <Splash hideSplash={this.hideSplash} />
+        }
+        {/* {!this.state.viewSplash &&
+          <AppManager>
+            <CameraFrame />
+          </AppManager>
+        } */}
+        {!this.state.viewSplash &&
+          <Reviews />
+        }
       </div>
     );
   }
