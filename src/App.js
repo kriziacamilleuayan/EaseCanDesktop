5d1f0687893f7db04a1c37e6425fe638ab05ff13
kcNeko <kriziacamilleuayan@gmail.com>
2019-07-21 09:12:51 +0800

    ingredients

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import React, { Component } from "react";
-import Reviews from './components/review/Reviews'
+import Reviews from "./components/review/Reviews";
 import "./App.css";
 
 import CameraFrame from "./components/camera/CameraFrame";
 import AppManager from "./components/appmanager/AppManager";
 import Spinner from "./components/spinner/Spinner";
-import Splash from './components/splash/Splash'
-import Reviews from './components/reviews/Reviews'
+import Splash from "./components/splash/Splash";
+// import Reviews from './components/reviews/Reviews'
 
 export default class App extends Component {
   state = {
-    viewSplash: true
-  }
+    viewSplash: true,
+    hasDetected: false
+  };
   hideSplash = () => {
-    this.setState({ viewSplash: false })
-  }
+    this.setState({ viewSplash: false });
+  };
+
+  setHasDetected = flag => {
+    this.setState({ hasDetected: flag });
+  };
+
   render() {
     return (
       <div className="App">
-
-        {this.state.viewSplash &&
-          <Splash hideSplash={this.hideSplash} />
-        }
+        {this.state.viewSplash && <Splash hideSplash={this.hideSplash} />}
         {/* {!this.state.viewSplash &&
           <AppManager>
             <CameraFrame />
           </AppManager>
         } */}
-        {!this.state.viewSplash &&
-            <AppManager>
-              <CameraFrame/>
-              <Reviews/>
-            </AppManager>
-        }
+        {!this.state.viewSplash && (
+          <AppManager setHasDetected={this.setHasDetected}>
+            <CameraFrame />
+            {this.state.hasDetected ? <Reviews /> : null}
+          </AppManager>
+        )}
       </div>
     );
   }
